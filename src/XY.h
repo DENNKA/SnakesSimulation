3a01a21a0b41431ceac489bbf11c9cef4ef1d8ec
DENNKA <spiridonov.denis34@gmail.com>
2020-05-08 14:10:54 +0400

    add overloading operators + - * /

--- src/XY.h
+++ src/XY.h
@@ -6,8 +6,12 @@
 struct XY{
     XY(){};
     template <typename T>
-    XY(sf::Vector2<T> v2) : x(v2.x), y(v2.y){};
+    XY(sf::Vector2<T> v2) : x(v2.x), y(v2.y){}
     XY(int x, int y) : x(x), y(y){}
+    XY operator +(const XY& xy){return XY(this->x + xy.x, this->y + xy.y);}
+    XY operator -(const XY& xy){return XY(this->x - xy.x, this->y - xy.y);}
+    XY operator *(const XY& xy){return XY(this->x * xy.x, this->y * xy.y);}
+    XY operator /(const XY& xy){return XY(this->x / xy.x, this->y / xy.y);}
     int x, y;
 };
 
